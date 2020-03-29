af4547ff21ceacadc5a35d3b82315663fc6b0343
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-03-30 00:22:43 +0100

    Providing live data with axios

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+// axios.get('https://api.covid19api.com/country/nigeria/status/confirmed/live')
+//   .then((response) => response.data)
+//     .then((res) =>{ 
+//       const currentData =  res[res.length - 1];
+
+//     } )
+//   ;
+
+axios.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=NG')
+  .then((response) => response)
+    .then((res) =>{ 
+         const currentData = res.data;
+         console.log(currentData);
+        let tagger =  document.getElementById('info_data');
+          tagger.innerHTML += `
+         <div class="col-md-3 stretch-card grid-margin">
+                <div class="card bg-gradient-danger card-img-holder text-white">
+                  <div class="card-body">
+                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <h4 class="font-weight-normal mb-3">Confirmed Cases<i class="mdi mdi-chart-line mdi-24px float-right"></i>
+                    </h4>
+                    <h2 class="mb-5">${currentData.latest.confirmed}</h2>
+                    <h6 class="card-text">Increased by </h6>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 stretch-card grid-margin">
+                <div class="card bg-gradient-info card-img-holder text-white">
+                  <div class="card-body">
+                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <h4 class="font-weight-normal mb-3">Deaths <i class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
+                    </h4>
+                    <h2 class="mb-5">${currentData.latest.deaths}</h2>
+                    <h6 class="card-text">Decreased by 10%</h6>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 stretch-card grid-margin">
+                <div class="card bg-gradient-success card-img-holder text-white">
+                  <div class="card-body">
+                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <h4 class="font-weight-normal mb-3">Recoveries <i class="mdi mdi-diamond mdi-24px float-right"></i>
+                    </h4>
+                    <h2 class="mb-5">${currentData.latest.recovered}</h2>
+                    <h6 class="card-text">Increased by 5%</h6>
+                  </div>
+                </div>
+              </div>
+         `
+    } ).catch(()=>{
+        tagger.innerHTML += `<img src="../images/spinner.gif"/>`;
+    });
